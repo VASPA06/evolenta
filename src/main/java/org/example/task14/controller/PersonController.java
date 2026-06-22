1825cd1fbe4d1f73ad5aed1e0442fbc2697bcb28
vaspa <parkaev0@mail.ru>
2026-06-22 09:21:50 +0300

    исправлены класс PersonController

--- src/main/java/org/example/task14/controller/PersonController.java
+++ src/main/java/org/example/task14/controller/PersonController.java
@@ -7,23 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
 
 @RestController
 public class PersonController {
     @Autowired
     private PersonRepository repository;
-    private final List<Person> persons = new ArrayList<>(Arrays.asList(
-            new Person(1, "Ivan", "Ivanovich", "Ivanov", LocalDate.of(1999, 2,3)),
-            new Person(2, "Петр", "Петрович", "Петров", LocalDate.of(2002, 2,2)),
-            new Person(3, "Евгений", "Васильевич", "Васин", LocalDate.of(2005, 4,8)),
-            new Person(4, "Максим", "Яковлевич", "Окопский", LocalDate.of(1978, 6,5))
-    ));
+
     @GetMapping("/person")
     public Iterable<Person> getPersons() {
         return repository.findAll();
